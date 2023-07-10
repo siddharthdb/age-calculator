852e49f2fe9594a57ee2c8a2352644391a075ba8
siddharthdb <siddharthdb@gmail.com>
2023-07-10 12:13:17 -0400

    fixed missing alt

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -4,7 +4,7 @@ import arrow from "../../../assets/images/icon-arrow.svg";
 const Button = ({ handleClick }) => {
   return (
     <div className="cmp-age-calc__Button" tabIndex={0} onClick={handleClick}>
-      <img src={arrow} />
+      <img src={arrow} alt="Calculate age" />
     </div>
   );
 };
